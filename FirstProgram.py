77adba71cb4204aa8ef6c4e4fc6c8a3cce58f2c1
sandeep.kumar <sandeep.rjil@outlook.com>
2024-02-24 17:23:14 +0530

    this is my python selenium project for learning

---
+++ FirstProgram.py
@@ -0,0 +1,29 @@
+print("Hello")
+a = 5
+print(a)
+a, b, c = 8, 9, 11.5
+print(a + b + c)
+# used for comment
+# = 5
+# b = "sandy"
+# print(type(b))
+ab = [3, 6, 11, "Sandeep", 55, "kumar"]
+
+bc = (22, 55, 'quick', 5)
+
+dd = {1, 4, 5, 0, 'ku', 33}
+
+print(type(ab))
+
+# if else
+sand = 56
+if sand == 56:
+    print("matching")
+else:
+    print("not matchis")
+
+
+
+
+str = "Sandeep chaudhary"
+
